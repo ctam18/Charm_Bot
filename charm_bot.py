7f7beb62cd52e449e064f425683cc4e6211c723b
ctam18 <18150@my.sanctamaria.school.nz>
2023-06-26 09:25:37 +1200

    Added comments for each section
    Comments for each section

--- charm_bot.py
+++ charm_bot.py
@@ -1,2 +1,69 @@
 # Charm bot program 
 
+# Welcome message with random name
+
+
+
+
+
+
+# List for click and collect or delivery
+
+
+
+
+
+
+# CLick and collect information - name and phone number
+
+
+
+
+
+
+# Delivery information - name address and phone
+
+
+
+
+
+# Choose total number of Charms - max 5
+
+
+
+
+
+
+# Charm List 
+
+
+
+
+
+
+# Charm order - from list - print each charm oreder with cost
+
+
+
+
+
+# Print order out - including if order is del or click and collect and names and prices of each charm - total cost including any delivery charge
+
+
+
+
+# Ability to cancel or proceed with order
+
+
+
+
+
+# Option for new order or to exit
+
+
+
+
+
+
+
+# Main Function
