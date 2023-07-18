6904de46c5f640355a04db1dca675040303444b6
ctam18 <18150@my.sanctamaria.school.nz>
2023-07-19 10:40:59 +1200

    Created new_exit function
    Created function for new_exit and integrated into main function

--- charm_bot.py
+++ charm_bot.py
@@ -3,6 +3,7 @@
 #Bugs - phone number input allows letters
 #     - Name input allows numbers
 
+import sys
 import random 
 from random import randint
 
@@ -212,6 +213,34 @@ def confirm_cancel():
 
 
 # Option for new order or to exit
+def new_exit():
+    print ("Do you want to start another Order or Exit")
+    print ("To start another order enter 1")
+    print ("To exit the BOT please enter 2")
+    while True:
+        try:
+            confirm = int(input("Please enter a number "))
+            if confirm >= 1 and confirm <= 2:
+                if confirm == 1:
+                    print ("New Order")
+                    order_list.clear()
+                    order_cost.clear()
+                    customer_details.clear()
+                    main()
+                    break
+
+                elif confirm == 2:
+                    print ("Exit")
+                    order_list.clear()
+                    order_cost.clear()
+                    customer_details.clear()
+                    sys.exit
+                    break
+            else: 
+                print("Number must be 1 or 2")
+        except ValueError:
+            print("That is not a valid number")
+            print("Please enter 1 or 2")
 
 
 
