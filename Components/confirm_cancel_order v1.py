1714125d30052a225587320b49892c7199c7ddae
ctam18 <18150@my.sanctamaria.school.nz>
2023-07-18 13:04:13 +1200

    Updated confirm_cancel_order
    Changed the print comments to informing customers whether their orders have been confirmed or cancelled

--- Components/confirm_cancel_order v1.py
+++ Components/confirm_cancel_order v1.py
@@ -7,10 +7,13 @@ while True:
         if confirm >= 1 and confirm <= 2:
             if confirm == 1:
                 print ("Order Confirmed")
+                print ("Your Order has been sent to our shop")
+                print ("Your Jewelry Charms will be with you shortly")
                 break
 
             elif confirm == 2:
-                print ("Order Cancelled")
+                print ("Your Order as been Cancelled")
+                print ("You can restart your order or exit the BOT")
                 break
         else: 
             print("Number must be 1 or 2")
