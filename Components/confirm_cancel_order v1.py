af84c3f533b8f811294984eb35bf6c4b40b52ffb
ctam18 <18150@my.sanctamaria.school.nz>
2023-07-18 12:46:05 +1200

    Changed variable
    Changed the delivery variable to confirm variable.

--- Components/confirm_cancel_order v1.py
+++ Components/confirm_cancel_order v1.py
@@ -3,14 +3,14 @@ print ("To confirm pleaser enter 1")
 print ("To cancel please enter 2")
 while True:
     try:
-        delivery = int(input("Please enter a number "))
-        if delivery >= 1 and delivery <= 2:
-            if delivery == 1:
+        confirm = int(input("Please enter a number "))
+        if confirm >= 1 and confirm <= 2:
+            if confirm == 1:
                 print ("Click and collect")
                 break
 
-            elif delivery == 2:
-                print ("Delivery")
+            elif confirm == 2:
+                print ("Confirm")
                 break
         else: 
             print("Number must be 1 or 2")
